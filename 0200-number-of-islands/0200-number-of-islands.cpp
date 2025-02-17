82ff29cd1be9898e9a6d9a8de307c72a3ad6c1f2
Naresh Tiwari <112651607+Nareshtiwari74@users.noreply.github.com>
2025-02-17 10:38:16 +0530

    Time: 25 ms (72.81%), Space: 16.2 MB (89.23%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -9,20 +9,21 @@ public:
             for(int j=0;j<n;j++){
                 if(grid[i][j]=='1'){
                     count++;
-                    dfs(grid,i,j,m,n);
+                    dfs(grid,i,j);
                 }
             }
         }
         return count;
     }
     private:
-     void dfs(vector<vector<char>>& grid, int i, int j, int m, int n) {
+     void dfs(vector<vector<char>>& grid, int i, int j) {
+         int m=grid.size();int n=grid[0].size();
         if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == '0') return;
 
         grid[i][j]='0';
-        dfs(grid,i+1,j,m,n);
-        dfs(grid,i-1,j,m,n);
-        dfs(grid,i,j+1,m,n);
-        dfs(grid,i,j-1,m,n);
+        dfs(grid,i+1,j);
+        dfs(grid,i-1,j);
+        dfs(grid,i,j+1);
+        dfs(grid,i,j-1);
     }
 };
